41f9af2db90df37749d88c8e4cb9690aca47a84b
cynthiawilliamsa <cynthiawilliamsa@gmail.com>
2018-11-01 14:50:22 -0500

    initial commit

--- server/index.js
+++ server/index.js
@@ -1,6 +1,20 @@
+//This is the starting point of the server, put all code here
+
 let express = require("express");
+//Add body parser middleware to express
+let bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+app.use(express.static('public'));
 let comments = require("./comments");
 let products = require("./products");
 let vehicles = require("./vehicles");
 let contacts = require("./contacts");
 
+//* Make your app listen to port 3001
+app.listen(3001, (err) => {
+if (err) {
+  return console.log("Error", err);
+}
+console.log("Web server is now living in apartment 3001");
+});
